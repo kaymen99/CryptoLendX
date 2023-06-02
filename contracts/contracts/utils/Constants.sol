c8cff7e93bedb19c5bd76f843701dba4f27577f3
Aymen <AymenMir1001@gmail.com>
2023-06-02 21:22:39 +0100

    Added contracts utils

---
+++ contracts/contracts/utils/Constants.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+abstract contract Constants {
+    uint256 internal constant PRECISION = 1e5;
+    uint256 internal constant RATE_PRECISION = 1e18;
+    uint256 internal constant LIQUIDATION_THRESHOLD = 8e4; // 80%
+    uint256 internal constant LIQUIDATION_CLOSE_FACTOR = 5e4; // 50%
+    uint256 internal constant LIQUIDATION_REWARD = 5e3; // 5%
+    uint256 internal constant MIN_HEALTH_FACTOR = 1e18;
+
+    // Default Interest Rate (if borrows = 0)
+    uint64 internal constant DEFAULT_INTEREST = 158247046; // 0.5% annual rate 1e18 precision
+
+    // Protocol Fee (1e5 precision)
+    uint16 internal constant DEFAULT_PROTOCOL_FEE = 0;
+    uint256 internal constant MAX_PROTOCOL_FEE = 1e4; // 10%
+
+    uint256 public constant BLOCKS_PER_YEAR = 2102400; // Average Ethereum blocks per year
+}
